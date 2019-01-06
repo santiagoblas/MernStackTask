8aedbfd03bfade4e82c6c890130d0ec5818d196f
Santiago Blas Beitia <santiagob.beitia@gmail.com>
2019-01-06 19:22:11 -0300

    Primer commit: levantar server y primera ruta más html index en static files. Minuto 40:24

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const router  = require('./routes/tasks.routes');
+const app = express();
+
+//#region SETTINGS ----------
+app.set('port', process.env.PORT || 3000)
+//#endregion
+
+//#region MIDDLEWARES -------
+app.use(morgan('dev'));
+app.use(express.json());
+//#endregion
+
+//#region ROUTES ------------
+app.use('/api/tasks', router);
+//#endregion
+
+//#region STATIC FILES ------
+app.use(express.static(path.join(__dirname, 'public')));
+//#endregion
+
+//#region STARTING SERVER ---
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
+//#endregion
+
